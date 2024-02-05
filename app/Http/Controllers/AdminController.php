d8877d4820b78413c138a1651ec240b905706874
Mario D <mdadurka@gmail.com>
2024-02-05 17:51:22 +0200

    profile photo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -42,6 +42,36 @@ class AdminController extends Controller
 
         return view('admin.admin_profile_view', compact('profileData'));
 
+    }
+
+    public function AdminProfileStore(Request $request) {
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            // @unlink(public_path('upload/admin_images' . $data->photo)) ---mano prideta kintamajojo vardas $deleted, nes kitaip nekesidavo nuotraukos
+            $deleted = @unlink(public_path('upload/admin_images/' . $data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'Administratoriaus profilis atnaujintas sėkmingai!',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
     }//End method
 
 } 
--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -16,7 +16,7 @@
             </nav>
         </div>
         <div class="ms-auto">
-            
+
         </div>
     </div>
     <!--end breadcrumb-->
@@ -39,88 +39,99 @@
                             <hr class="my-4" />
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe me-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
+                                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe me-2 icon-inline">
+                                            <circle cx="12" cy="12" r="10"></circle>
+                                            <line x1="2" y1="12" x2="22" y2="12"></line>
+                                            <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+                                        </svg>Website</h6>
                                     <span class="text-secondary">https://codervent.com</span>
                                 </li>
-                             
+
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-8">
                     <div class="card">
-                        <div class="card-body">
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Name</h6>
+
+                        <form method="POST" action="{{ route('admin.profile.store') }}" enctype="multipart/form-data">
+                            @csrf
+
+                            <div class="card-body">
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Name</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <input type="text" name='name' class="form-control" value="{{ $profileData->name }}" />
+                                    </div>
                                 </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" name='name' class="form-control" value="{{ $profileData->name }}" />
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">User Name</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <input type="text" name='username' class="form-control" value="{{ $profileData->username }}" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">User Name</h6>
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Email</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <input type="email" name='email' class="form-control" value="{{ $profileData->email }}" />
+                                    </div>
                                 </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" name='username' class="form-control" value="{{ $profileData->username }}" />
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Phone</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <input type="text" name='phone' class="form-control" value="{{ $profileData->phone }}" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Email</h6>
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Address</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <input type="text" name='address' class="form-control" value="{{ $profileData->address }}" />
+                                    </div>
                                 </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="email" name='email' class="form-control" value="{{ $profileData->email }}" />
+
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Profile Image</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <input type="file" name='photo' class="form-control" id="image" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Phone</h6>
+
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0"></h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <img id='showImage' src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="70" height="70">
+                                    </div>
                                 </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" name='phone' class="form-control" value="{{ $profileData->phone }}" />
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Address</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" name='address' class="form-control" value="{{ $profileData->address }}" />
+
+                                <div class="row">
+                                    <div class="col-sm-3"></div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
+                                    </div>
                                 </div>
                             </div>
 
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Profile Image</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="file" name='photo' class="form-control" id="image"  />
-                                </div>
-                            </div>
+                        </form>
 
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0"></h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <img id='showImage' src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="70" height="70">
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-sm-3"></div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="button" class="btn btn-primary px-4" value="Save Changes" />
-                                </div>
-                            </div>
-                        </div>
                     </div>
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="card">
-                                
+
                             </div>
                         </div>
                     </div>
@@ -131,11 +142,10 @@
 </div>
 
 <script type="text/javascript">
-
-    $(document).ready(function(){
-        $('#image').change(function(e){
+    $(document).ready(function() {
+        $('#image').change(function(e) {
             var reader = new FileReader();
-            reader.onload = function(e){
+            reader.onload = function(e) {
                 $('#showImage').attr('src', e.target.result);
             }
             reader.readAsDataURL(e.target.files['0']);
@@ -145,4 +155,3 @@
 </script>
 
 @endsection
-
